01dce50118bdbabd8d0b138422b6eb6399f3a1d6
premjiGramaner <114988098+premjiGramaner@users.noreply.github.com>
2023-01-16 16:20:26 +0530

    Update constants.js

--- server/config/constants.js
+++ server/config/constants.js
@@ -4,6 +4,6 @@ module.exports = Object.freeze({
     CONFIG: {
         PORT: 3001,
     },
-    JWT_SECRET_KEY: "mtfest-jwt@2020#",
-    CRYPTR_SECRET_KEY: "mtfest-transfer-2020"
+    JWT_SECRET_KEY: "test-jwt@2020#",
+    CRYPTR_SECRET_KEY: "test-transfer-2020"
 });
