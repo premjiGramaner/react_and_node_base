8ce95c65c0147b5757961a61833483bc385e81b2
gunasekaran0810 <89788016+bunnyguna8@users.noreply.github.com>
2023-02-06 15:22:28 +0530

    button component code updated

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -4,11 +4,9 @@ import { IButtonInterface } from '@Interface/ComponentsInterface/ButtonInterface
 const Button: React.FC<IButtonInterface> = props => {
   const { handleButtonClick, className, children } = props
   return (
-    <div>
-      <button className={`border-0 ${className}`} onClick={handleButtonClick}>
-        {children}
-      </button>
-    </div>
+    <button className={`border-0 ${className}`} onClick={handleButtonClick}>
+      {children}
+    </button>
   )
 }
 
