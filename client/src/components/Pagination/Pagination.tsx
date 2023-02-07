89c178c9cd4f5ceff04e614fae6d4589299f3e68
gunasekaran0810 <89788016+bunnyguna8@users.noreply.github.com>
2023-02-08 05:22:34 +0530

    Table & Pagination code updated

--- client/src/components/Pagination/Pagination.tsx
+++ client/src/components/Pagination/Pagination.tsx
@@ -1,57 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { usePagination, DOTS } from './usePagination'
 
-const Pagination = ({ nPages, currentPage, setCurrentPage, totalRecords }) => {
-  const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
+const Pagination = props => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount,
+    currentPage,
+    pageSize,
+    className,
+  } = props
 
-  const nextPage = () => {
-    if (currentPage !== nPages) setCurrentPage(currentPage + 1)
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  })
+  const [handlePageCount, setHandlePageCount] = useState<number>(currentPage)
+
+  if (currentPage === 0 || paginationRange?.length < 2) {
+    return null
   }
-  const prevPage = () => {
-    if (currentPage !== 1) setCurrentPage(currentPage - 1)
+
+  const onNext = () => {
+    onPageChange(currentPage + 1)
   }
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1)
+  }
+  const onFirst = () => {
+    onPageChange(1)
+  }
+  const onLast = () => {
+    onPageChange(lastPage)
+  }
+
+  const handlePage = e => {
+    setHandlePageCount(e.target.value)
+  }
+
+  let lastPage = paginationRange[paginationRange?.length - 1]
   return (
-    <nav className="pagination-container d-flex justify-content-end align-items-center">
-      <ul className="pagination justify-content-center ">
-        <li className="page-item">
-          <a
-            className="page-link"
-            href="#"
-            aria-label="Previous"
-            onClick={prevPage}
-          >
-            <span aria-hidden="true">&laquo;</span>
-            <span className="sr-only">Previous</span>
-          </a>
+    <div className="pagination-wrapper d-flex justify-content-end align-items-center">
+      <ul className={`pagination-container ${className}`}>
+        <li className={`pagination-item `} onClick={onFirst}>
+          <div className="arrow left" />
         </li>
-        {pageNumbers.map(pgNumber => (
-          <li
-            key={pgNumber}
-            className={`page-item ${currentPage == pgNumber ? 'active' : ''} `}
-          >
-            <a
-              onClick={() => setCurrentPage(pgNumber)}
-              className="page-link"
-              href="#"
+        <li
+          className={`pagination-item ${currentPage === 1 && 'disabled'} `}
+          onClick={onPrevious}
+        >
+          <div className="arrow left" />
+        </li>
+        {paginationRange.map(pageNumber => {
+          if (pageNumber === DOTS) {
+            return <li className="pagination-item dots">&#8230;</li>
+          }
+
+          return (
+            <li
+              className={`pagination-item ${
+                pageNumber === currentPage && 'selected'
+              }`}
+              onClick={() => onPageChange(pageNumber)}
             >
-              {pgNumber}
-            </a>
-          </li>
-        ))}
-        <li className="page-item">
-          <a
-            className="page-link"
-            href="#"
-            aria-label="Next"
-            onClick={nextPage}
-          >
-            <span aria-hidden="true">&raquo;</span>
-            <span className="sr-only">Next</span>
-          </a>
+              {pageNumber}
+            </li>
+          )
+        })}
+        <li
+          className={`pagination-item ${
+            currentPage === lastPage && 'disabled'
+          }`}
+          onClick={onNext}
+        >
+          <div className="arrow right" />
+        </li>
+        <li className={`pagination-item `} onClick={onLast}>
+          <div className="arrow right" />
         </li>
       </ul>
-      <input type="number" className="page-count" value={nPages} />
-      <p className="pagination-total-count">{`${currentPage}-${nPages} of ${totalRecords?.length}`}</p>
-    </nav>
+      <input
+        type="number"
+        className="page-count mx-3"
+        value={handlePageCount}
+        onChange={e => handlePage(e)}
+      />
+      <p className="pagination-total-count">{`${currentPage}-${pageSize} of ${totalCount}`}</p>
+    </div>
   )
 }
 
--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { ITableInterface } from '@Utils/interface/ComponentsInterface'
 import { getValueFromObject } from '@Utils/utils'
 
 const Table: React.FC<ITableInterface> = props => {
   const { column, rowContent } = props
+  const [order, setOrder] = useState<string>('ASC')
+  const [tableData, setTableData] = useState<any[]>(rowContent)
+
   const TableRowCell = ({ tabelData, tableHeader }) => {
     const value = getValueFromObject(tabelData, tableHeader.key)
     return (
@@ -13,6 +16,22 @@ const Table: React.FC<ITableInterface> = props => {
         </span>
       </td>
     )
+  }
+  const sorting = (col: string) => {
+    if (order === 'ASC') {
+      const sorted = [...rowContent].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      )
+      setTableData(sorted)
+      setOrder('DSC')
+    }
+    if (order === 'DSC') {
+      const sorted = [...rowContent].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      )
+      setTableData(sorted)
+      setOrder('ASC')
+    }
   }
   return (
     <div>
@@ -25,7 +44,11 @@ const Table: React.FC<ITableInterface> = props => {
             <thead>
               <tr>
                 {column.map((_col, _colIdx) => (
-                  <th scope="col" key={_colIdx}>
+                  <th
+                    scope="col"
+                    key={_colIdx}
+                    onClick={() => sorting(_col.key)}
+                  >
                     <span className="table-header d-flex align-items-center justify-content-center">
                       {_col.name}
                       {_col.isSort && <i className="bi bi-sort-down-alt"></i>}
