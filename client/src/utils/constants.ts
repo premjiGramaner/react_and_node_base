c3dd9ad999fcd9a6824090e6eff3dcc8b8582fad
gunasekaran0810 <89788016+bunnyguna8@users.noreply.github.com>
2023-05-16 11:04:47 +0530

    version-1.0.13 updated & uat end point changes

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://168.62.172.215/api/' // uat
+export const API_URL = 'http://74.235.243.35/api/' // uat
 // export const API_URL = 'http://98.70.0.57/api/' // staging
 // export const API_URL = 'http://localhost:5001/api/'; //local
 
