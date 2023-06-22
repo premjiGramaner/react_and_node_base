04329249c78d053fe2b51758acbfcaea8eaa3ca6
premji-2606 <premkumar.t@gramenerit.com>
2023-06-22 19:43:19 +0530

    Version update 1.0.16

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,7 +1,7 @@
-export const API_URL = 'http://168.62.172.215/api/' // prod
+// export const API_URL = 'http://168.62.172.215/api/' // prod
 // export const API_URL = 'http://74.235.243.35/api/' // uat
 // export const API_URL = 'http://98.70.0.57/api/' // staging
-// export const API_URL = 'http://localhost:5001/api/'; //local
+export const API_URL = 'http://localhost:5002/api/'; //local
 
 export const STORAGE_KEY = {
   AUTH_TOKEN: 'accessToken',
