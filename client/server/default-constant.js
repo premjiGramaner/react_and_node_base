ed0128910e7bab36c105d6ce0e3c1c66d18f35a1
premkumar.t <premkumar.t@thinkpalm.com>
2023-08-01 16:12:59 +0530

    Version update

--- client/server/default-constant.js
+++ client/server/default-constant.js
@@ -2,13 +2,13 @@ module.exports = {
     API_Version: "v1/",
     API_URL: "https://zedcontrol.gmwtus.zededa.net/api/",
     CLUSTER_INFO: {
-        "Gmwtus": "zedcontrol.gmwtus.zededa.net",
-        "Pmwtus": "zedcontrol.pmwtus.zededa.net",
-        "Prod": "zedcontrol.zededa.net",
-        "Orchid-d3vw": "zedcontrol.orchid-d3vw.com",
-        "Vmware": "ecs-deo.vmware.com",
-        "Rockwell-dev": "edgecontrol.cloud-dev.rockwellautomation.com",
-        "Rockwell": "edgecontrol.cloud.rockwellautomation.com",
-        "Agora": "zedcontrol.agora.zededa.net"
+        "gmwtus": "zedcontrol.gmwtus.zededa.net",
+        "pmwtus": "zedcontrol.pmwtus.zededa.net",
+        "prod": "zedcontrol.zededa.net",
+        "orchid-d3vw": "zedcontrol.orchid-d3vw.com",
+        "vmware": "ecs-deo.vmware.com",
+        "rockwell-dev": "edgecontrol.cloud-dev.rockwellautomation.com",
+        "rockwell": "edgecontrol.cloud.rockwellautomation.com",
+        "agora": "zedcontrol.agora.zededa.net"
     }
 }
