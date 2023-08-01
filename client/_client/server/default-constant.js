18ea8f082afb51ab1e97e59fed0a6c5542c57dce
premkumar.t <premkumar.t@thinkpalm.com>
2023-08-01 21:51:27 +0530

    code clean up

--- client/_client/server/default-constant.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    API_Version: "v1/",
-    API_URL: "https://zedcontrol.zededa.net/api/",
-}
--- client/_client/server/helpers/redis-list.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    userInfo: "ZEA__USER_INFO",
-    projects: "ZEA__PROJECTS",
-    devices: "ZEA__DEVICES",
-    instances: "ZEA__INSTANCES",
-    user_events: "ZEA__USER_EVENTS",
-};
