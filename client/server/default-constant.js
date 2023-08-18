28025e9ace4aa026ff3ae1c5def42b94e335ba1b
premkumar.t <premkumar.t@thinkpalm.com>
2023-08-18 16:12:05 +0530

    quick fix: default constants

--- client/server/default-constant.js
+++ client/server/default-constant.js
@@ -2,13 +2,13 @@ module.exports = {
     API_Version: "v1/",
     API_URL: "https://zedcontrol.gmwtus.zededa.net/api/",
     CLUSTER_INFO: {
-        "gmwtus": "zedcontrol.gmwtus.zededa.net",
-        "pmwtus": "zedcontrol.pmwtus.zededa.net",
-        "prod": "zedcontrol.zededa.net",
-        "orchid-d3vw": "zedcontrol.orchid-d3vw.com",
-        "vmware": "ecs-deo.vmware.com",
-        "rockwell-dev": "edgecontrol.cloud-dev.rockwellautomation.com",
-        "rockwell": "edgecontrol.cloud.rockwellautomation.com",
-        "agora": "zedcontrol.agora.zededa.net"
+        "zedcontrol.gmwtus.zededa.net": "zedcontrol.gmwtus.zededa.net",
+        "zedcontrol.pmwtus.zededa.net": "zedcontrol.pmwtus.zededa.net",
+        "zedcontrol.zededa.net": "zedcontrol.zededa.net",
+        "zedcontrol.orchid-d3vw.com": "zedcontrol.orchid-d3vw.com",
+        "ecs-deo.vmware.com": "ecs-deo.vmware.com",
+        "edgecontrol.cloud-dev.rockwellautomation.com": "edgecontrol.cloud-dev.rockwellautomation.com",
+        "edgecontrol.cloud.rockwellautomation.com": "edgecontrol.cloud.rockwellautomation.com",
+        "zedcontrol.agora.zededa.net": "zedcontrol.agora.zededa.net"
     }
 }
