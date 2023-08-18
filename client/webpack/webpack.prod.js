18a3bdbf84bd5bb822b5b7e95fe6b2e3dc465ea3
premkumar.t <premkumar.t@thinkpalm.com>
2023-08-18 11:42:38 +0530

    feat: Fav icon update

--- client/webpack/webpack.prod.js
+++ client/webpack/webpack.prod.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack')
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'production',
@@ -6,6 +8,11 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('react'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, '..', './src/favicon-zededa.ico'), to: path.resolve(__dirname, '..', "./build") }
+      ]
     }),
   ],
 }
